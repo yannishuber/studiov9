56f3b02a04118359234caa550152a1212a85606a
Yannis Huber <yannis.h@hotmail.ch>
2019-07-07 14:01:00 +0200

    Version 1

--- header.php
+++ header.php
@@ -7,20 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="<?php echo get_bloginfo('description'); ?>">
 
+    
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script type="module" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule="" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.js"></script>
-    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <title><?php echo get_bloginfo('name'); ?></title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    
     <link href="<?php echo get_bloginfo('template_directory'); ?>/font/stylesheet.css" rel="stylesheet">
     <link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+    <title><?php echo get_bloginfo('name'); ?></title>
+    
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -31,6 +30,9 @@
         $(() => {
 
             $('img').removeAttr('title');
+            $('.carousel').carousel({
+                interval: 4000
+            })
 
             $(".menu-icon").on("click", () => {
                 $(".mobile-nav-content").toggleClass("show");
@@ -62,13 +64,15 @@
 
     <div class="mobile-nav-content">
         <div class="bg">
-            <span class="menu-icon" id="close"><ion-icon name="close"></ion-icon></span>
+            <span class="menu-icon" id="close">
+                <ion-icon name="close"></ion-icon>
+            </span>
             <?php
-                        wp_nav_menu(array(
-                            'menu' => 'sv9-menu',
-                            'container_class' => 'sv9-menu'
-                        ));
-                        ?></div>
+            wp_nav_menu(array(
+                'menu' => 'sv9-menu',
+                'container_class' => 'sv9-menu'
+            ));
+            ?></div>
     </div>
 
     <div class="title-bar">
--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php get_header(); ?>
-	<!--<php get_template_part( 'content', get_post_format() ); ?>-->
-  <?php /*echo do_shortcode('[metaslider id="5"]');*/ ?>
+<!--<php get_template_part( 'content', get_post_format() ); ?>-->
+<?php /*echo do_shortcode('[metaslider id="5"]');*/ ?>
 
-  <div class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide">
+<div class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <?php $args = array(
+            'post_type' => 'attachment',
+            'category_name' => 'acceuil',
+        );
+        $first = True;
+        $attachments = get_posts($args);
+        foreach ($attachments as $i) : ?>
+            <div class="carousel-item <?php if($first) {$first = False; echo 'active';} ?>" style="background-image: url('<?php echo $i->guid; ?>')"></div>
+        <?php endforeach; ?>
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
-    </div>
-  </div>
 </div>
+
 <?php get_footer('nocontact'); ?>
