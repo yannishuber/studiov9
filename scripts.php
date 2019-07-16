7b62962f78689d2d1bc6d60a4d0a898c9d05cf85
Yannis Huber <yannis.h@hotmail.ch>
2019-07-16 18:52:34 +0200

    Finishing up

--- scripts.php
+++ scripts.php
@@ -56,7 +56,7 @@
     }
 
     const scrollToTop = () => {
-        const c = document.documentElement.scrollTop || document.body.scrollTop;
+        const c = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop)
         if (c > 0) {
             window.requestAnimationFrame(scrollToTop);
             window.scrollTo(0, c - c / 8);
