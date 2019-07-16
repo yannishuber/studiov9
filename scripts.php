868604c488c96528ff91764dd018271e9eb31d7e
Yannis Huber <yannis.h@hotmail.ch>
2019-07-16 18:19:39 +0200

    Remove bootstrap and jquery

--- scripts.php
+++ scripts.php
@@ -6,7 +6,6 @@
 
     function init() {
         document.getElementById("open").addEventListener('click', function(e) {
-            // prevent compatibility mouse events and click
             document.getElementById("mobile-nav-content").classList.add("show");
             e.stopPropagation();
             e.preventDefault();
@@ -14,7 +13,6 @@
 
         });
         document.getElementById("close").addEventListener('click', function(e) {
-            // prevent compatibility mouse events and click
             document.getElementById("mobile-nav-content").classList.remove("show");
             e.stopPropagation();
             e.preventDefault();
@@ -30,15 +28,16 @@
     function update_images() {
 
         var slides;
+        var current = 1;
 
         if (window.innerWidth > 600) {
             slides = document.getElementsByClassName("desktop");
+            current = 1;
         } else {
             slides = document.getElementsByClassName("mobile");
+            current = 1;
         }
 
-        var current = 1;
-        
         slides[0].style.opacity = 1;
         setInterval(function() {
             for (var i = 0; i < slides.length; i++) {
