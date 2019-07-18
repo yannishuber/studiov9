ed82de841b5b4535a0bb0086fed9c1ce04175586
Yannis Huber <yannis.h@hotmail.ch>
2019-07-18 18:55:51 +0200

    Cleaned the code

--- static/js/index.js
+++ static/js/index.js
@@ -1,46 +1,62 @@
+// 0 = not set, 1 = desktop, 2 = mobile  
+var device = 0;
+
 const update_images = () => {
 
-    const callback = function() {
-        console.log(this.responseText)
+    if (device == 0 || (device == 1 && window.innerWidth < 600) || (device == 2 && window.innerWidth > 600)) {
 
-        JSON.parse(this.responseText).forEach(src => {
+        const callback = function() {
+            console.log(this.responseText)
 
-            var div = document.createElement('div');
-            div.style.backgroundImage = "url('" + src + "')"
-            div.classList.add('slider-item')
-            document.getElementById('home-images').appendChild(div)
-        })
+            // Remove old images
+            var els = document.querySelectorAll(".slider-item")
 
-        // Remove loader
-        let loader = document.getElementById('home-loader')
-        loader.parentNode.removeChild(loader)
+            if (els != null) Array.prototype.forEach.call(els, function(e) { e.parentNode.removeChild(e) })
 
-        var current = 1
+            JSON.parse(this.responseText).forEach(src => {
 
-        var slides = document.getElementsByClassName('slider-item')
+                var div = document.createElement('div');
+                div.style.backgroundImage = "url('" + src + "')"
+                div.classList.add('slider-item')
+                document.getElementById('home-images').appendChild(div)
+            })
 
-        if (slides.length < 1) return
+            // Remove loader
+            let loader = document.getElementById('home-loader')
+            if (loader != null) loader.parentNode.removeChild(loader)
+
+            var current = 1
+
+            var slides = document.querySelectorAll(".slider-item")
+
+            if (slides.length < 1) return
 
 
-        setTimeout(() => {
-            slides[0].style.opacity = 1
-        }, 100)
+            setTimeout(() => {
+                slides[0].style.opacity = 1
+            }, 100)
 
-        document.getElementById('title-bar').classList.add('white')
+            document.getElementById('title-bar').classList.add('white')
 
-        setInterval(() => {
-            slides[(current - 1) < 0 ? slides.length - 1 : current - 1].style.opacity = 0
-            current = (current + 1) % slides.length
-            slides[current].style.opacity = 1
-        }, 10000)
+            setInterval(() => {
+                slides[(current - 1) < 0 ? slides.length - 1 : current - 1].style.opacity = 0
+                current = (current + 1) % slides.length
+                slides[current].style.opacity = 1
+            }, 10000)
+        }
+
+        device = (window.innerWidth > 600) ? 1 : 2;
+
+
+        let req = new XMLHttpRequest()
+        req.onload = callback
+        req.open('POST', wpurl.ajaxurl, true)
+        req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+        req.send('action=images&device=' + (device == 1 ? 'desktop' : 'mobile'))
     }
-
-    let req = new XMLHttpRequest()
-    req.onload = callback
-    req.open('POST', wpurl.ajaxurl, true)
-    req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-    req.send('action=images&device=' + ((window.innerWidth > 600) ? 'desktop' : 'mobile'))
-
 }
+
+window.onresize = update_images
+window.onorientationchange = update_images
 
 update_images()
