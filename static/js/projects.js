9234b44f6c72cacadb22d336e172339d23991798
Yannis Huber <yannis.h@hotmail.ch>
2019-07-25 22:54:02 +0200

    display projects smoothly

--- static/js/projects.js
+++ static/js/projects.js
@@ -1,5 +1,5 @@
 const proj = () => {
-    document.querySelectorAll('.project-item-image').forEach((e) => e.style.opacity = 1)
+    document.querySelectorAll('.project-item-image').forEach((e) => setTimeout(() => { e.style.opacity = 1 }, 100))
 }
 
 window.onload = proj
