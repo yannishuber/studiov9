77d2a69981f0a9d01db4875dcf2f565d30769e53
Yannis Huber <yannis.h@hotmail.ch>
2019-07-30 14:58:00 +0200

    removed cities

--- footer-home.php
+++ footer-home.php
@@ -1,5 +1,5 @@
 <footer id="home-footer">
-    <span id="home-info">Biel/Bienne et Vevey</span>
+    <span id="home-info"></span>
     <div id="social">
         <a id="facebook" target="_blank" href="https://facebook.com/STUDIOV9architectes">FACEBOOK</a>
         <a id="instagram" target="_blank" href="https://instagram.com/studiov9">INSTAGRAM</a>
