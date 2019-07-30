ee7988bbc50073d199b8a6065af303221dd8b097
Yannis Huber <yannis.h@hotmail.ch>
2019-07-30 14:44:14 +0200

    Adapted footers for mobile devices

--- footer.php
+++ footer.php
@@ -3,15 +3,17 @@
         <a id="facebook" target="_blank" href="https://facebook.com/STUDIOV9architectes">FACEBOOK</a>
         <a id="instagram" target="_blank" href="https://instagram.com/studiov9">INSTAGRAM</a>
     </div>
-    <div id="footer-biel">
-        <p class="addr">Dammweg 3</p>
-        <p class="addr">2502 Biel/Bienne</p>
-        <p><a href="mailto:info@studiov9.ch">info@studiov9.ch</a></p>
-    </div>
-    <div id="footer-vevey">
-        <p class="addr">Rue du Théâtre 1</p>
-        <p class="addr">1800 Vevey</p>
-        <p><a href="mailto:rouiller@studiov9.ch">rouiller@studiov9.ch</a></p>
+    <div id="adresses">
+        <div id="footer-biel">
+            <p class="addr">Dammweg 3</p>
+            <p class="addr">2502 Biel/Bienne</p>
+            <p><a href="mailto:info@studiov9.ch">info@studiov9.ch</a></p>
+        </div>
+        <div id="footer-vevey">
+            <p class="addr">Rue du Théâtre 1</p>
+            <p class="addr">1800 Vevey</p>
+            <p><a href="mailto:rouiller@studiov9.ch">rouiller@studiov9.ch</a></p>
+        </div>
     </div>
     <span id="copyright">STUDIOV9 <?php echo date('Y'); ?> - Tout droits réservés</span>
     <img id="arrow" src="<?php echo get_bloginfo('template_directory'); ?>/static/arrow.png" width="25px" />
