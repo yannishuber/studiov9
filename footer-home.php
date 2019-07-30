5a89d454c15c6ffae8a0bd1622af7a9ace3344f1
Yannis Huber <yannis.h@hotmail.ch>
2019-07-30 14:12:14 +0200

    Version3 finished

--- footer-home.php
+++ footer-home.php
@@ -1,9 +1,9 @@
-<footer id="home-footer" >
+<footer id="home-footer">
     <span id="home-info">Biel/Bienne et Vevey</span>
     <div id="social">
         <a id="facebook" target="_blank" href="https://facebook.com/STUDIOV9architectes">FACEBOOK</a>
         <a id="instagram" target="_blank" href="https://instagram.com/studiov9">INSTAGRAM</a>
-</div>
+    </div>
 </footer>
 </div> <!-- content-area -->
 <?php wp_footer(); ?>
